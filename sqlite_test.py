168ed3c4627f27c2ec418e801d2466bde4ae0202
Karan Kyanam <karan.kyanam@gmail.com>
2017-02-11 18:17:48 -0800

    Created sample db with sqlite

---
+++ sqlite_test.py
@@ -0,0 +1,18 @@
+#create a SQLite3 database and table
+
+#import the sqlite3 library
+import sqlite3
+
+#creates a new database if the datbase doesn't exist already
+conn = sqlite3.connect("new.db")
+
+#get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+#create a table
+cursor.execute("""CREATE TABLE population
+					(city TEXT, state TEXT, population INT)
+				""")
+
+#close the database connection
+conn.close()
